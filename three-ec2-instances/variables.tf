a28d58a2a04545f1fbf8d511ecef31287656458f
lingaiah-ctu <sivalingapractise555@gmail.com>
2025-06-05 22:50:32 +0530

    expense ansible

---
+++ three-ec2-instances/variables.tf
@@ -0,0 +1,18 @@
+variable "allow_all" {
+   type = string
+   default = "sg-09c7c70bd56f0d58b"
+}
+variable "zone_id" {
+    default = "Z07608533HHBQHW47XU3F"
+}
+variable "domain_name" {
+  default = "lingaiah.online"
+}
+variable "instances" {
+    type     = map
+    default = {
+        mysql     = "t2.small"
+        backend   = "t2.micro"
+        frontend  = "t2.micro"
+    }
+}
